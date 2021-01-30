3eb1b57fa81539a6fe038b2e835da73fc0cf8be4
trevor clements <trevortheamazing@gmail.com>
2021-01-30 04:33:33 -0800

    init commit

---
+++ BlazorMovies/Shared/Entities/Movie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Shared.Entities
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Poster { get; set; }
+        public string TitleBrief 
+        { 
+            get
+            {
+                if (string.IsNullOrEmpty(Title))
+                {
+                    return null;
+                }
+                if (Title.Length > 60)
+                {
+                    return Title.Substring(0, 60) + "...";
+                }
+                else
+                {
+                    return Title;
+                }
+            }
+        }
+    }
+}
